8a39489aaf60621218db84655cc70f0ab3fb3994
antongoransson <anton.goransson95@gmail.com>
2017-03-28 20:25:04 +0200

    Proj

--- ComputerProj1/Main.py
+++ ComputerProj1/Main.py
@@ -2,6 +2,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
 
 def geval(l,b,d):
     L=np.zeros(6)
@@ -10,14 +11,13 @@ def geval(l,b,d):
     h= getH(L,p)
     x = getXPCord(b,d,p)
     y = getYPCord(b,d,p)
-    print(p,h,x,y)
 
 def getP(L,b,d):
     P = np.zeros(3)
     for i in range(3):
         P[i]= (1/(2*b))*(b**(2) + L[2*i]**(2)-L[2*i+1]**(2))
     return P
-##SQRT blir negativ för L=3
+
 def getH(L,P):
     h = np.zeros(3)
     for i in range(3):
@@ -26,7 +26,7 @@ def getH(L,P):
 
 def getXPCord(b,d,P):
     x = np.zeros(3)
-    x[0] =np.sqrt((3)/6)*(2*b+d-3*P[0])
+    x[0] = (np.sqrt(3)/6)*(2*b+d-3*P[0])
     x[1] = -(np.sqrt(3)/6)*(b+2*d)
     x[2] = -(np.sqrt(3)/6)*(b-d-3*P[2])
     return x
@@ -41,11 +41,68 @@ def getYPCord(b,d,p):
 def getMinLegLength(b,d):
     return b/2
 
-def Task1():
+def stf1(xt):
+    e=np.zeros(3)                                                  #ROW 2 IN EQUATION
+    e[0]= a**(2)+ 2*xt[0]*xt[1]-2*xt[0]*(xp[0]+np.sqrt(3)*(yp[0]-yp[1]))-2*xp[1]*xt[1]-((np.sqrt(3)*xp[0]-yp[0]+yp[1])**(2)+h[0]**(2)+h[1]**(2)-4*xp[0]**(2)-xp[1]**(2))+ getThirdRow(1,xt,xp,h)
+                                                                                #ROW 2 IN EQUATION                                  #ROW 3 IN EQUATION
+    e[1]= a**(2)-4*xt[0]*xt[2]-2*xt[0]*(xp[0]-3*xp[2]+np.sqrt(3)*(yp[0]-yp[2]))-2*xt[2]*(-3*xp[0]+xp[2]+np.sqrt(3)*(yp[0]-yp[2])) -((np.sqrt(3)*(xp[0]+xp[2])-yp[0]+yp[2])**(2)+(h[0]**(2)+h[2]**(2))-4*xp[0]**(2)-4*xp[2]**(2))+getThirdRow(2,xt,xp,h)
+                                                                        #ROW 2 IN EQUATION
+    e[2] = a**(2) + 2*xt[1]*xt[2]-2*xt[2]*(xp[2]+np.sqrt(3)*(yp[1]-yp[2]))-((np.sqrt(3)*xp[2]-yp[1]+yp[2])**(2)+(h[1]**(2)+h[2]**(2))-xp[1]**(2)-4*xp[2]**(2))+ getThirdRow(3,xt,xp,h)
+
+    return e
+
+
+def stf(x,a,b,d,L,p,h,xp,yp):
+    xt1=x[0]
+    xt2=x[1]
+    xt3=x[2]
+    F=np.zeros(3)                                                  #ROW 2 IN EQUATION
+    F[0]= a**2+ 2*xt1*xt2-2*xt1*(xp[0]+np.sqrt(3)*(yp[0]-yp[1]))-2*xp[1]*xt2-((np.sqrt(3)*xp[0]-yp[0]+yp[1])**(2)+h[0]**(2)+h[1]**(2)-4*xp[0]**(2)-xp[1]**(2))+ 2*np.sqrt((h[0]**(2)-4*(xt1-xp[0])**(2))*(h[1]**(2)-(xt2-xp[1])**(2)))
+                                                                            #ROW 2 IN EQUATION                                  #ROW 3 IN EQUATION
+    F[1]= a**(2)-4*xt1*xt3-2*xt1*(xp[0]-3*xp[2]+np.sqrt(3)*(yp[0]-yp[2]))-2*xt3*(-3*xp[0]+xp[2]+np.sqrt(3)*(yp[0]-yp[2])) -((np.sqrt(3)*(xp[0]+xp[2])-yp[0]+yp[2])**(2)+(h[0]**(2)+h[2]**(2))-4*xp[0]**(2)-4*xp[2]**(2))+2*np.sqrt((h[0]**(2)-4*(xt1-xp[0])**(2))*(h[2]**(2)-4*(xt3-xp[2])**(2)))
+                                                                        #ROW 2 IN EQUATION
+    F[2]= a**(2) + 2*xt2*xt3-2*xt3*(xp[2]+np.sqrt(3)*(yp[1]-yp[2]))-((np.sqrt(3)*xp[2]-yp[1]+yp[2])**(2)+(h[1]**(2)+h[2]**(2))-xp[1]**(2)-4*xp[2]**(2))+ 2*np.sqrt((h[1]**(2)-(xt2-xp[1])**(2))*(h[2]**(2)-4*(xt3-xp[2])**(2)))
+
+    return F
+
+def getThirdRow(i,xt,xp,h):
+    row =0
+    if i==1:
+        row=2*np.sqrt((h[0]**(2)-4*(xt[0]-xp[0])**(2))*(h[1]**(2)-(xt[1]-xp[1])**(2)))
+    elif i==2:
+        row=2*np.sqrt((h[0]**(2)-4*(xt[0]-xp[0])**(2))*(h[2]**(2)-4*(xt[2]-xp[2])**(2)))
+    else:
+        row=2*np.sqrt((h[1]**(2)-(xt[1]-xp[1])**(2))*(h[2]**(2)-4*(xt[2]-xp[2])**(2)))
+    return row
+
+def plotCurve(f):
+    x = np.linspace(-10,10,num=1000)
+    fig, ax = plt.subplots()
+    ax.plot(x,f(xt))
+    #ax.set_aspect('equal')
+    ax.grid(True, which='both')
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
+    plt.show()
+def task1():
     print(getMinLegLength(15,1))
     geval(8,15,1)
 
+def task2():
+    # 4<XT1<8    -4<XT2<4.72       0.45<XT3<5.45
+    a=10
+    b=15
+    d=1
+    L = np.zeros(6)
+    L.fill(11.5)
+    p = getP(L,b,d)
+    h= getH(L,p)
+    xp = getXPCord(b,d,p)
+    yp = getYPCord(b,d,p)
+    x0 = np.array([4,-3,4])
+    x = fsolve(stf,x0,(a,b,d,L,p,h,xp,yp))
+    print(x)
+    print (stf(x,a,b,d,L,p,h,xp,yp))
 
-def guessXT(a,b,d,xt,p,h,XP,YP):
-    e1= a**(2) + 2*xt[0]*xt[1]-2*xt[0]*(xp[0]+np.sqrt(3)(*yp[0]-yp[1]))
-     -2*xp[1]*xt[1]-((np.sqrt(3)*xp[0]-yp[0]+yp[1])**(2)+h[0]**(2)+h[1]**(2)-4*xp[0]**(2)-xp[1]**(2))
+task1()
+task2()
